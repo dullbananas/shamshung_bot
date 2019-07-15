7b3176382c126d6d832365bc554c069848792726
Dull Bananas <dull.bananas0@gmail.com>
2019-07-15 20:52:21 +0000

    Add prefix system

--- shamshung_bot/client.py
+++ shamshung_bot/client.py
@@ -1,5 +1,7 @@
 import discord
 import logging
+import shlex
+from . import db, cmds
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,4 +15,15 @@ class ShamshungBot(discord.Client):
 	
 	
 	async def on_message(self, msg):
-		print(f'Message from {msg.author}: {msg.content}')
+		if msg.author.id == self.user.id:
+			return
+		
+		logging.info(f'Message from {msg.author}: {msg.content}')
+		session = db.Session()
+		prefix = 'shamshung.'
+		for i in session.query(db.Prefix).filter(Prefix.server_id == msg.guild.id):
+			prefix = i.prefix
+		
+		if msg.content.startswith(prefix):
+			args = shlex.split(msg.content[len(prefix):])
+			await msg.channel.send(repr(args))
--- shamshung_bot/db.py
+++ shamshung_bot/db.py
@@ -1,2 +1,15 @@
 import os
-import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, BigInteger, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine(os.getenv('SHAMSHUNG_DB_URL'))
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+
+class Prefix(Base):
+	__tablename__ = 'prefixes'
+	server_id = Column(BigInteger, primary_key=True)
+	prefix = Column(String(32))
